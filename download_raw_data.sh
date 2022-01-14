75b9485b8670788d5d036003d1b5a2d861ae58ad
dtork@dolores <dtorkelson@gmail.com>
2022-01-14 15:49:08 +0000

    add script to get data

---
+++ download_raw_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Get and extract raw data from original source
+
+wget --output-file='r4.2.tar.bz2' https://kilthub.cmu.edu/ndownloader/files/24856766
+tar -xjvf 'r4.2.tar.bz2'
+
+# The result is a folder, r4.2, containing the CSVs. Move them into the root of the repository
+# for the original script to work.
